204e4d944303d3f4dc4c000c4a792ac49a2d409f
ungtsuhan <ungtsuhan@gmail.com>
2022-03-17 16:36:47 +0800

    create UserService and register it with scoped life time

---
+++ webapi/webapi/Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿namespace webapi.Services
+{
+    public interface IUserService
+    {
+        bool IsValidUserCredentials(string? userName, string? password);
+    }
+
+    public class UserService : IUserService
+    { 
+        private readonly IDictionary<string, string> _users = new Dictionary<string, string>
+        {
+            { "test1", "password1" },
+            { "test2", "password2" },
+            { "admin", "securePassword" }
+        };
+
+        public bool IsValidUserCredentials(string? userName, string? password)
+        {
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                return false;
+            }
+
+            return _users.TryGetValue(userName, out var p) && p == password;
+        }
+    }
+}
