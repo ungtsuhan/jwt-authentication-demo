5f1a7c21314ef909d2688e850a39db3c60d8ff65
ungtsuhan <ungtsuhan@gmail.com>
2022-03-17 16:37:44 +0800

    create login api for authentication

--- webapi/webapi/Controllers/AccountController.cs
+++ webapi/webapi/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using webapi.Services;
 
 namespace webapi.Controllers
 {
@@ -6,5 +9,38 @@ namespace webapi.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly IUserService _userService;
+
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("login")]
+        public ActionResult Login([FromBody] LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            if (!_userService.IsValidUserCredentials(request.UserName, request.Password))
+            {
+                return Unauthorized();
+            }
+
+            return Ok();
+        }
+
+        public class LoginRequest
+        {
+            [Required]
+            [JsonPropertyName("username")]
+            public string? UserName { get; set; }
+
+            [Required]
+            [JsonPropertyName("password")]
+            public string? Password { get; set; }
+        }
     }
 }
