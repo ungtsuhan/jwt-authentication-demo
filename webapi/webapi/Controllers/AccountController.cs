6c7ef8884a43f3e25c5264cf9c05ee9cfefa5e29
ungtsuhan <ungtsuhan@gmail.com>
2022-03-17 16:12:10 +0800

    create AccountController

---
+++ webapi/webapi/Controllers/AccountController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+    }
+}
