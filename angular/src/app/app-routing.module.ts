19a489abb2d09a35e9af386b2b1286b2dc046b94
ungtsuhan <ungtsuhan@gmail.com>
2022-03-18 13:18:52 +0800

    implement auth guard and protect route

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { WeatherComponent } from './weather/weather.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: '', pathMatch: 'full', component: HomeComponent,canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
-  { path: 'weather', component: WeatherComponent },
+  { path: 'weather', component: WeatherComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
