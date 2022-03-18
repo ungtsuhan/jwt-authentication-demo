cff57877bd58c4908132c6f76398bc762c80b165
ungtsuhan <ungtsuhan@gmail.com>
2022-03-18 17:46:20 +0800

    design weather component

--- angular/src/app/weather/weather.component.ts
+++ angular/src/app/weather/weather.component.ts
@@ -2,6 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
+interface Weather {
+  date: Date;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
@@ -10,6 +17,11 @@ import { environment } from 'src/environments/environment';
 export class WeatherComponent implements OnInit {
 
   private readonly apiUrl = `${environment.apiUrl}WeatherForecast`;
+  weathers: Weather[] = [];
+  weathersPaginated: Weather[] = [];
+  page = 1;
+  pageSize = 4;
+  collectionSize = this.weathers.length;
 
   constructor(private http: HttpClient) { }
 
@@ -21,8 +33,15 @@ export class WeatherComponent implements OnInit {
       if(!environment.production) {
         console.log(res);
       }
+      this.weathers = res as Weather[];
+      this.refreshWeathers();
     });
   }
 
-
+  refreshWeathers() {
+    this.weathersPaginated = this.weathers
+      .map((weather, i) => ({id: i + 1, ...weather}))
+      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+    this.collectionSize = this.weathers.length;
+  }
 }
