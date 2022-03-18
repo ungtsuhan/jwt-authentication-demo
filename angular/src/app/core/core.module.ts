3520d1a01baf9dc61ec6a03e7b9bb5d8016d87cd
ungtsuhan <ungtsuhan@gmail.com>
2022-03-18 14:50:40 +0800

    implment jwt interceptor

--- angular/src/app/core/core.module.ts
+++ angular/src/app/core/core.module.ts
@@ -1,11 +1,13 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { UnauthorizedInterceptor } from './interceptors/unauthorized.interceptor';
+import { JWTInterceptor } from './interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [],
   imports: [],
   providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JWTInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: UnauthorizedInterceptor, multi: true },
   ],
 })
---
+++ angular/src/app/core/interceptors/jwt.interceptor.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { 
+    HttpEvent, 
+    HttpHandler, 
+    HttpInterceptor, 
+    HttpRequest 
+} from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class JWTInterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const token = localStorage.getItem('access_token');
+        if (token) {
+            req = req.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+        }
+        return next.handle(req);
+    }
+}
